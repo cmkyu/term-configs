c1f14d6d7d0251c29e9dc0a363c581967474c85f
Chang Yu <marcus.yu.56@gmail.com>
2026-09-10 10:59:14 -0700

    Disable auto formatting.

--- nvim/lua/config/conform.lua
+++ nvim/lua/config/conform.lua
@@ -1,15 +1,9 @@
--- Format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
-
 require("conform").setup({
   formatters_by_ft = {
     c = {"clang-format"},
     cpp = {"clang-format"},
   },
+
+  -- format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 })
 
